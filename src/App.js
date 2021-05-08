f56d9e6404f860699b8c779dd6e830dfb016d383
omkar <borudeomkar41@gmail.com>
2021-05-08 14:16:50 +0530

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
 import {Routes,Route} from "react-router-dom";
-import {API} from "./API"
 import { useEffect } from 'react';
-import { useStateProvider } from "./Context/MainContext/StateProvider"
-import {Navbar} from "./Components/Navbar/Navbar"
-import {Home} from "./Components"
-import {Login,Profile,SignUp,useAuth,Cart,WishList} from "./Components/index";
-import {ProductListing} from "./Components/ProductListing/productlisting"
+import {PrivateRoute} from "./PrivateRoute";
+import {Login,Home,Navbar,API,useStateProvider,Toast,Profile,ProductListing,SignUp,useAuth,Cart,WishList} from "./Components/index";
 import {getApiProduct} from "./ApiCalls/api-calls"
-import {Toast} from "./Components/Toast/Toast";
 function App() {
   const { isUserloggedIn, userId } = useAuth();
   const {dispatch,state} = useStateProvider();
@@ -74,9 +69,9 @@ function App() {
         <Route path="/products" element={<ProductListing />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/account" element={<Profile/>} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/wishlist" element={<WishList/>} />
+        <PrivateRoute path="/account" element={<Profile/>} />
+        <PrivateRoute path="/cart" element={<Cart />} />
+        <PrivateRoute path="/wishlist" element={<WishList/>} />
       </Routes>
     </div>
   );
