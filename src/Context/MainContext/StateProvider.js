3069f7f941da37432f5d3d0c296c2437e0feeebd
omkar <borudeomkar41@gmail.com>
2021-06-04 17:54:16 +0530

    Refactor

--- src/Context/MainContext/StateProvider.js
+++ src/Context/MainContext/StateProvider.js
@@ -1,38 +1,33 @@
 import { createContext, useContext, useReducer, useState } from "react";
-import {stateReducer} from "./ProductContext"
-
+import { stateReducer } from "./stateReducer";
 
 const StateContext = createContext();
 
-export function StateProvider({children}) {
-
-    
- 
-    
-     
-    const [state,dispatch] = useReducer(stateReducer,{
-        products: [],
-        itemsInWishlist: [],
-        itemsInCart: [],
-        SortBy:"",
-        showFastDelivery:false,
-        ProductPriceRange:2000,   
-        dataFilter: {
-            includeOutOfStock: true,
-            filterByBrands: [],
-          },
-        toast: {
-            value:false,
-            message:""
-        }
-    
-    });
-    return (
-      <StateContext.Provider value={{state,dispatch}}>
-            {children}
-        </StateContext.Provider>
-    );
+export function StateProvider({ children }) {
+  const [state, dispatch] = useReducer(stateReducer, {
+    products: [],
+    itemsInWishlist: [],
+    itemsInCart: [],
+    userOrders: [],
+    userAddresses: [],
+    userSelectedAddress: [],
+    SortBy: "",
+    showFastDelivery: false,
+    ProductPriceRange: 2000,
+    dataFilter: {
+      includeOutOfStock: true,
+      filterByBrands: [],
+    },
+    toast: {
+      value: false,
+      message: "",
+    },
+  });
+  return (
+    <StateContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StateContext.Provider>
+  );
 }
 
-export const useStateProvider = () => 
-     useContext(StateContext);
+export const useStateProvider = () => useContext(StateContext);
