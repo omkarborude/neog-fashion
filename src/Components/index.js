73524fb18c14f61c2ef90cfd0a1a653604888763
omkar <borudeomkar41@gmail.com>
2021-06-04 17:53:56 +0530

    Update

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,12 +1,16 @@
-export {Home} from "./Home/home"
-export {Navbar} from "./Navbar/Navbar"
-export {ProductListing} from "./ProductListing/productlisting"
-export {useStateProvider} from  "../Context/MainContext/StateProvider"
-export {useAuth} from "../Context/AuthContext/AuthProvider"
-export {Login} from "./Auth/Login"
-export {SignUp} from "./Auth/SignUp"
-export {Profile} from "./Acccount/Profile"
-export {Cart} from "./Cart/Cart"
-export {API} from "../API"
-export {Toast} from "./Toast/Toast"
-export {WishList} from "./wishlist/WishList"
+export { Home } from "./Home/home";
+export { Navbar } from "./Navbar/Navbar";
+export { ProductListing } from "./ProductListing/productlisting";
+export { useStateProvider } from "../Context/MainContext/StateProvider";
+export { useAuth } from "../Context/AuthContext/AuthProvider";
+export { Login } from "./Auth/Login";
+export { SignUp } from "./Auth/SignUp";
+export { Profile } from "./Acccount/Profile";
+export { Cart } from "./Cart/Cart";
+export { API } from "../API";
+export { Toast } from "./Toast/Toast";
+export { WishList } from "./wishlist/WishList";
+export { AddNewAddress } from "./Address/AddNewAddress";
+export { MyAddresses } from "./Address/MyAddresses";
+export { MyOrders } from "./MyOrders/MyOrders";
+export { ProudctDetails } from "./ProductDetails/ProductDetails";
