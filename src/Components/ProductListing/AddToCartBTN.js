1cfeb23d1abe449f5404b7d0d33c87f2d613a55f
omkar <borudeomkar41@gmail.com>
2021-06-04 17:54:58 +0530

    Refactor

--- src/Components/ProductListing/AddToCartBTN.js
+++ src/Components/ProductListing/AddToCartBTN.js
@@ -1,54 +1,63 @@
 import { useNavigate } from "react-router-dom";
-import { useEffect, useRef } from "react";
-import {useAuth} from "../index";
-import {useStateProvider} from "../index";
-import {AddProductToCart,AddProductToWishlist} from "../../ApiCalls/api-calls";
-import {isAlreadyExist} from "../../ApiCalls/api-calls";
+import { useAuth } from "../index";
+import { useStateProvider } from "../index";
+import {
+  AddProductToCart,
+  AddProductToWishlist,
+} from "../../ApiCalls/api-calls";
+import { isAlreadyExist } from "../../ApiCalls/api-calls";
 import "./productlisting.css";
 
-export function AddToCartBTN({product}) {
+export function AddToCartBTN({ product }) {
+  const { state, dispatch } = useStateProvider();
+  const { isUserloggedIn, userId } = useAuth();
+  const navigate = useNavigate();
 
-    const { state, dispatch } = useStateProvider();
-    const { isUserloggedIn, userId } = useAuth();
-    const navigate = useNavigate();
+  return (
+    <>
+      <button
+        className="btn-addtocart"
+        onClick={() => {
+          isUserloggedIn
+            ? isAlreadyExist(state?.itemsInCart, product._id)
+              ? navigate("/cart")
+              : AddProductToCart({
+                  state,
+                  dispatch,
+                  product,
+                  userId,
+                })
+            : navigate("/login");
+        }}
+      >
+        {isAlreadyExist(state?.itemsInCart, product._id)
+          ? "Go To Cart"
+          : "Add TO Cart"}
+      </button>
 
-
-    return (
-        <>
-           <button className="btn-addtocart"
-           onClick={()=> {
-               isUserloggedIn ? isAlreadyExist(state.itemsInCart,product._id)
-               ?  navigate("/cart") : AddProductToCart({
-                  
-                   state,
-                   dispatch,
-                   product,
-                   userId
-               }) : navigate("/login")
-           }}
-           >
-               add to cart
-           </button>
-
-           <div className="addtowishlist-btn-div">
-              
-               
-               <button className="btn-addtowishlist"
-               onClick={()=> {
-                isUserloggedIn ? isAlreadyExist(state.itemsInWishlist,product._id)
-                ?  navigate("/wishlist") : AddProductToWishlist({
-                   
+      <div className="addtowishlist-btn-div">
+        <button
+          className="btn-addtowishlist"
+          onClick={() => {
+            isUserloggedIn
+              ? isAlreadyExist(state.itemsInWishlist, product._id)
+                ? navigate("/wishlist")
+                : AddProductToWishlist({
                     state,
                     dispatch,
                     product,
-                    userId
-                }) : navigate("/login")
-            }}
-               >
-               <i 
-                class='bx bx-bookmark-heart' ></i>
-             </button>
-           </div>
-        </>
-    )
+                    userId,
+                  })
+              : navigate("/login");
+          }}
+        >
+          {isAlreadyExist(state.itemsInWishlist, product._id) ? (
+            <i class="fab fa-gratipay"></i>
+          ) : (
+            <i class="far fa-heart"></i>
+          )}
+        </button>
+      </div>
+    </>
+  );
 }
