3400fb120cd5c498a27eeb965f9e1e783d65b500
omkar <borudeomkar41@gmail.com>
2021-06-04 17:54:38 +0530

    Feat: Product page

--- src/Components/wishlist/WishList.jsx
+++ src/Components/wishlist/WishList.jsx
@@ -1,76 +1,82 @@
-import { useNavigate } from "react-router-dom";
-import {useStateProvider} from "../index"
+import { useNavigate, Link } from "react-router-dom";
+import { useStateProvider } from "../index";
 import "./wishlist.css";
-import {AddProductToCart,isAlreadyExist,RemoveProductWishlist} from "../../ApiCalls/api-calls"
-import {useAuth,Toast} from "../index";
-
-
+import {
+  AddProductToCart,
+  isAlreadyExist,
+  RemoveProductWishlist,
+} from "../../ApiCalls/api-calls";
+import { useAuth, Toast } from "../index";
 
 export const WishList = () => {
-const {state,dispatch} = useStateProvider();
-const { isUserloggedIn, userId } = useAuth();
-const navigate = useNavigate();
+  const { state, dispatch } = useStateProvider();
+  const { isUserloggedIn, userId } = useAuth();
+  const navigate = useNavigate();
 
-    return (
+  return (
+    <div className="wishlist-main-div">
+      {state.itemsInWishlist.length === 0 ? (
+        <h1 className="cart-title">Empty</h1>
+      ) : (
+        <h1 className="cart-title">My Wishlist</h1>
+      )}
 
-        <div className="wishlist-main-div">
-             
-            {state.itemsInWishlist.length === 0 ? <h1 className="cart-title">Empty</h1> : <h1 className="cart-title">My Wishlist</h1> }
-        
-                {state.itemsInWishlist.map(({ productId: product })=> {
+      {state.itemsInWishlist.map(({ productId: product }) => {
+        return (
+          <div className="wishlist-card-div">
+            <div className="wishlist-card-img-div">
+              <img src={product.image} className="wishlist-img" />
+            </div>
 
-                    return (
-                        <div className="wishlist-card-div">
-                           <div className="wishlist-card-img-div">
-                               <img src={product.image} 
-                               className="wishlist-img"
-                               />
-                            </div>
+            <div className="wishlist-card-info-div">
+              <Link to={`/${product._id}`} state={{ product }} className="Link">
+                <p className="cart-info-name">{product.name}</p>
+                <p className="cart-info-brand">
+                  Brand: <span>{product.brand}</span>
+                </p>
+                <p className="cart-info-rating">
+                  Rating:{" "}
+                  <span>
+                    {product.rating}
+                    <i class="bx bxs-star"></i>
+                  </span>
+                </p>
+                <p className="cart-info-price">
+                  Rs. {product.price} <span>({product.offer}%off)</span>
+                </p>
+              </Link>
 
-                        <div className="wishlist-card-info-div">
-                            <p className="cart-info-name">{product.name}</p>
-                            <p className="cart-info-brand">Brand: <span>{product.brand}</span></p>
-                            <p className="cart-info-rating">Rating: <span>{product.rating}<i class='bx bxs-star'></i></span></p>
-                            <p className="cart-info-price">Rs. {product.price} <span>({product.offer}%off)</span></p>
-                         
-                            <button 
-                            onClick={()=> {
-                                isUserloggedIn ? isAlreadyExist(state.itemsInCart,product._id)
-                                ?  navigate("/cart") : AddProductToCart({
-                                   
-                                    state,
-                                    dispatch,
-                                    product,
-                                    userId
-                                }) : navigate("/login")
-                            }}
-                            className="btn-movetocart">
-                                Add TO Cart
-                            </button>
+              <button
+                onClick={() => {
+                  isUserloggedIn
+                    ? isAlreadyExist(state.itemsInCart, product._id)
+                      ? navigate("/cart")
+                      : AddProductToCart({
+                          state,
+                          dispatch,
+                          product,
+                          userId,
+                        })
+                    : navigate("/login");
+                }}
+                className="btn-movetocart"
+              >
+                Add TO Cart
+              </button>
 
-                            <button 
-                            onClick={()=>{
-                                RemoveProductWishlist({state,
-                                    dispatch,
-                                    product,
-                                    userId})
-                            }}
-                            className="btn-movetocart">
-                                    Remove
-                                </button>
-                            </div>
-                            {state.toast.value && <Toast message = {state.toast.message}/>}
-
-
-                                
-
-                        </div> 
-
-
-                    )
-                })} 
-               
-      
-        </div>
-    )
-}
+              <button
+                onClick={() => {
+                  RemoveProductWishlist({ state, dispatch, product, userId });
+                }}
+                className="btn-movetocart"
+              >
+                Remove
+              </button>
+            </div>
+            {state.toast.value && <Toast message={state.toast.message} />}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
